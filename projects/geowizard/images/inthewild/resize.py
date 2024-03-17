39ea1ab07226aec673e9bbe2bc5294c04943f5e6
XiaoFu <lemonaddie0909@gmail.com>
2024-03-18 03:13:58 +0800

    update

--- projects/geowizard/images/inthewild/resize.py
+++ projects/geowizard/images/inthewild/resize.py
@@ -10,30 +10,30 @@ rgb = cv2.imread(rgb_file)
 h, w = rgb.shape[:2]
 cv2.imwrite(rgb_file, cv2.resize(rgb, (w_uni, int(h/560*h_uni)), interpolation=cv2.INTER_CUBIC)[100:800,:,:])
 
-ours_depth_file = file + 'ours_depth.jpg'
-ours_depth = cv2.imread(ours_depth_file)
-h, w = ours_depth.shape[:2]
-cv2.imwrite(ours_depth_file, cv2.resize(ours_depth, (w_uni,int(h/560*h_uni)), interpolation=cv2.INTER_CUBIC)[100:800,:,:])
+# ours_depth_file = file + 'ours_depth.jpg'
+# ours_depth = cv2.imread(ours_depth_file)
+# h, w = ours_depth.shape[:2]
+# cv2.imwrite(ours_depth_file, cv2.resize(ours_depth, (w_uni,int(h/560*h_uni)), interpolation=cv2.INTER_CUBIC)[100:800,:,:])
 
-ours_normal_file = file + 'ours_normal.jpg'
-ours_normal = cv2.imread(ours_normal_file)
-h, w = ours_normal.shape[:2]
-cv2.imwrite(ours_normal_file, cv2.resize(ours_normal, (w_uni,int(h/560*h_uni)), interpolation=cv2.INTER_CUBIC)[100:800,:,:])
+# ours_normal_file = file + 'ours_normal.jpg'
+# ours_normal = cv2.imread(ours_normal_file)
+# h, w = ours_normal.shape[:2]
+# cv2.imwrite(ours_normal_file, cv2.resize(ours_normal, (w_uni,int(h/560*h_uni)), interpolation=cv2.INTER_CUBIC)[100:800,:,:])
 
-marigold_file = file + 'marigold.jpg'
-marigold = cv2.imread(marigold_file)
-h, w = marigold.shape[:2]
-cv2.imwrite(marigold_file, cv2.resize(marigold, (w_uni,int(h/560*h_uni)), interpolation=cv2.INTER_CUBIC)[100:800,:,:])
+# marigold_file = file + 'marigold.jpg'
+# marigold = cv2.imread(marigold_file)
+# h, w = marigold.shape[:2]
+# cv2.imwrite(marigold_file, cv2.resize(marigold, (w_uni,int(h/560*h_uni)), interpolation=cv2.INTER_CUBIC)[100:800,:,:])
 
 omnidata_file = file + 'omnidata.jpg'
 omnidata = cv2.imread(omnidata_file)
 h, w = omnidata.shape[:2]
 cv2.imwrite(omnidata_file, cv2.resize(omnidata, (w_uni,int(h/560*h_uni)), interpolation=cv2.INTER_CUBIC)[100:800,:,:])
 
-depthany_file = file + 'depthany.jpg'
-depthany = cv2.imread(depthany_file)
-h, w = depthany.shape[:2]
-cv2.imwrite(depthany_file, cv2.resize(depthany, (w_uni,int(h/560*h_uni)), interpolation=cv2.INTER_CUBIC)[100:800,:,:])
+# depthany_file = file + 'depthany.jpg'
+# depthany = cv2.imread(depthany_file)
+# h, w = depthany.shape[:2]
+# cv2.imwrite(depthany_file, cv2.resize(depthany, (w_uni,int(h/560*h_uni)), interpolation=cv2.INTER_CUBIC)[100:800,:,:])
 
 dsine_file = file + 'dsine.jpg'
 dsine = cv2.imread(dsine_file)
